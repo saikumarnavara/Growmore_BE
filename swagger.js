4c4f0c192fce93217828800f1a59d3880295bc39
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-09-15 23:30:26 +0530

    updated swagger to prod connection

--- swagger.js
+++ swagger.js
@@ -1,18 +1,14 @@
 const swaggerAutogen = require("swagger-autogen")();
 
-const isProduction = process.env.NODE_ENV === "production" ? true : false;
-console.log({ isProduction });
-
 const doc = {
   info: {
     title: "GROWMORE API",
     description: "API Documentation for GROWMORE",
   },
-  host:
-    process.env.NODE_ENV === "production"
-      ? "growmore.vercel.app"
-      : "localhost:4001",
-  schemes: [process.env.NODE_ENV === "production" ? "https" : "http"],
+  host: "growmore-seven.vercel.app",
+  schemes: ["https"],
+  // host: "localhost:4001",
+  // schemes: ["http"],
 };
 
 const outputFile = "./swagger-output.json";
