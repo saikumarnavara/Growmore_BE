20110c84496d4b92da90279fb20089972a3dd18f
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-09-15 23:20:44 +0530

    updated swagger.js

--- swagger.js
+++ swagger.js
@@ -1,32 +1,18 @@
-// // swagger.js
-// const swaggerAutogen = require("swagger-autogen")();
-
-// const doc = {
-//   info: {
-//     title: "GROWMORE API",
-//     description: "API Documentation for GROWMORE",
-//   },
-//   host: "growmore-seven.vercel.app",
-//   schemes: ["https"],
-// };
-
-// const outputFile = "./swagger-output.json";
-// const endpointsFiles = ["./index.js"];
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-//   require("./index.js");
-// });
-
 const swaggerAutogen = require("swagger-autogen")();
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production" || true;
+console.log({ isProduction });
 
 const doc = {
   info: {
     title: "GROWMORE API",
     description: "API Documentation for GROWMORE",
   },
-  host: isProduction ? "growmore.vercel.app" : "localhost:4001",
-  schemes: [isProduction ? "https" : "http"],
+  host:
+    process.env.NODE_ENV === "production"
+      ? "growmore.vercel.app"
+      : "localhost:4001",
+  schemes: [process.env.NODE_ENV === "production" ? "https" : "http"],
 };
 
 const outputFile = "./swagger-output.json";
