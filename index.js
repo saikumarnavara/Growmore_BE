44c76f06af67bca473f223f06ee388f249160c32
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-09-15 22:48:35 +0530

    test prod

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 const mongoose = require("mongoose");
 const url = process.env.MONGO_URL;
